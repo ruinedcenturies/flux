71bc625228a5768be5ebc959c779987cb8beec7a
ruined <finducy@protonmail.com>
2025-08-17 18:24:14 -0500

    1

--- Flux/Client/ClientModule.lua
+++ Flux/Client/ClientModule.lua
@@ -1,1 +1,16 @@
 local ClientModule = {}
+
+-- Variables
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+ClientModule.Remotes = {}
+
+-- Dependencies
+local Listener = require(script.Listener)
+local PromiseWrapper = require(script.PromiseWrapper)
+local RemoteList = require(ReplicatedStorage.Flux.Shared.RemoteList)
+local RequestTypes = require(ReplicatedStorage.Flux.Shared.RequestTypes)
+
+-- Initialize Module
+function ClientModule.Init()
+    
+end
