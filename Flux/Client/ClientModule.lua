b05386a417cda8bd299171578546239edc9be4b3
ruined <finducy@protonmail.com>
2025-08-17 18:43:35 -0500

    createhandler

--- Flux/Client/ClientModule.lua
+++ Flux/Client/ClientModule.lua
@@ -8,9 +8,19 @@ local Shared = require(Flux.Shared.RemoteList)
 local RequestTypes = require(Flux.Shared.RequestTypes)
 local Listener = require(Flux.Client.Listener)
 local PromiseWrapper = require(Flux.Client.PromiseWrapper)
+local RemotesFolder = Config.RemotesFolder
+ClientModule.Remotes = {}
 
 -- Initialize Module
 function ClientModule.Init()
     -- Cache remotes
-    for Name, RemoteName
+    for Name, RemoteName in pairs(Shared) do
+        local Remote = RemotesFolder:WaitForChild("RemoteName")
+
+        if Remote then
+            self.Remotes[Name] = Remote
+        else
+            
+        end
+    end
 end
