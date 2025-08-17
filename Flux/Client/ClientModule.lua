4e6a8bc1af26c054e638a76686ee57abd5cbaf15
ruined <finducy@protonmail.com>
2025-08-17 18:39:56 -0500

    change replace

--- Flux/Client/ClientModule.lua
+++ Flux/Client/ClientModule.lua
@@ -3,7 +3,7 @@ local ClientModule = {}
 -- Variables/Dependencies
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Flux = ReplicatedStorage:WaitForChild("Flux")
-
+local Config = require(Flux.Shared.FluxConfig)
 local Shared = require(Flux.Shared.RemoteList)
 local RequestTypes = require(Flux.Shared.RequestTypes)
 local Listener = require(Flux.Client.Listener)
@@ -12,9 +12,5 @@ local PromiseWrapper = require(Flux.Client.PromiseWrapper)
 -- Initialize Module
 function ClientModule.Init()
     -- Cache remotes
-    for n, r in pairs(RemoteList) do
-        self.Remotes[n] = r
-    end
-
-    
+    for Name, RemoteName
 end
